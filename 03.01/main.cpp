26c623e1e33ede8e67b5e8a48de235f76641a88e
trainzug <philippschneeider@gmail.com>
2020-05-21 16:34:33 +0200

    praktikum 3

---
+++ 03.01/main.cpp
@@ -0,0 +1,103 @@
+#define CATCH_CONFIG_RUNNER
+#include <iostream>
+#include <fstream>
+#include "Tree.h"
+#include "catch.h"
+
+using namespace std;
+
+void printMenu ();
+void readCSV (Tree & tree);
+
+int main () {
+
+	int result = Catch::Session().run();
+	Tree tree;
+	string eingabe = "", name;
+	int alter, plz;
+	double einkommen;
+
+	do {
+		printMenu();
+		cout << "?> ";
+		cin >> eingabe;
+		if (eingabe == "1") {
+
+			cout << "Bitte geben Sie den Datensatz ein" << endl;
+			cout << "Name ?> ";
+			cin >> name;
+			cout << "Alter ?> ";
+			cin >> alter;
+			cout << "Einkommen ?> ";
+			cin >> einkommen;
+			cout << "PLZ ?> ";
+			cin >> plz;
+			tree.addNode(name, alter, einkommen, plz);
+			cout << "+ Ihr Datensatz wurde eingefuegt" << endl;
+		} else if (eingabe == "2") {
+			cout << "2 + Moechten Sie die Daten aus der Datei \"ExportZielanalyse.csv\" importieren (j/n) ?>";
+			cin >> eingabe;
+			if (eingabe == "j") {
+				readCSV(tree);
+			}
+		} else if (eingabe == "4") {
+			cout << "+ Bitte geben Sie den zu suchenden Datensatz an" << endl;
+			cout << "Name ?> ";
+			cin >> name;
+			tree.searchNode(name);
+		} else if (eingabe == "5") {
+			tree.printAll();
+		} else if (eingabe == "6") {
+			tree.printLevelOrder();
+		} else if (eingabe == "0") {
+			cout << "ok" << endl;
+			break;
+		} else {
+			cout << "Unbekannte Eingabe..." << endl;
+		}
+	}
+	while (true);
+
+	system("PAUSE");
+
+	return 0;
+}
+
+void printMenu () {
+	cout << "Menue:" << endl;
+	cout << "1) Datensatz einfuegen, manuell" << endl;
+	cout << "2) Datensatz einfuegen, csv" << endl;
+	cout << "4) Suchen" << endl;
+	cout << "5) Ausgabe in Preorder" << endl;
+	cout << "6) Ausgabe in Levelorder" << endl;
+}
+
+void readCSV (Tree & tree) {
+	fstream f("ExportZielanalyse.csv");
+	if (!f.is_open()) {
+		cout << "Fehler beim oeffnen von ExportZielanalyse.csv" << endl;
+		return;
+	}
+	string name, s;
+	int plz, alter;
+	double einkommen;
+	char sep = ';';
+
+	while (!f.eof()) {
+
+		s = "";
+		getline(f, s, sep);
+		if (s.empty())
+			break;
+		name = s;
+		getline(f, s, sep);
+		alter = stoi(s);
+		getline(f, s, sep);
+		einkommen = stod(s);
+		getline(f, s, '\n');
+		plz = stoi(s);
+		tree.addNode(name, alter, einkommen, plz);
+	}
+	f.close();
+	cout << "Daten wurden dem Baum hinzugefuegt" << endl;
+}
